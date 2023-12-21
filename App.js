4c24d18637550bd8ce28540764c4e374e142d79c
HyerinJeon <hyerin0930@gmail.com>
2023-12-21 17:36:27 +0900

    :sparkles: Icons

--- App.js
+++ App.js
@@ -8,11 +8,22 @@ import {
   ActivityIndicator,
 } from "react-native";
 import * as Location from "expo-location";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 // NOTE 해당 기기의 넓이값 가져오기
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const API_KEY = "145ac9e4880d44e525c972645ffa6a9d";
+
+const icons = {
+  Clouds: "cloudy-outline",
+  Clear: "sunny-outline",
+  Snow: "snow-outline",
+  Rain: "rainy-outline",
+  Drizzle: "md-rainy-outline",
+  Thunderstorm: "thunderstorm-outline",
+  Atmosphere: "cloudy-outline",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -53,46 +64,76 @@ export default function App() {
   }, []);
 
   // NOTE RN에서는 flexDirection 기본값이 column
-  return (
-    <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
+  if (!ok) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.icon}>
+          <FontAwesome5 name="sad-cry" size={100} color="white" />
+        </View>
       </View>
-      <ScrollView
-        // 가로 스크롤
-        horizontal
-        // 스크롤 시 하나씩 넘어가게 설정
-        pagingEnabled
-        // 가로 스크롤바 안보이게 설정
-        showsHorizontalScrollIndicator={false}
-        // 스크롤바 스타일링
-        // indicatorStyle="white"
-        contentContainerStyle={styles.weather}
-      >
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator color="white" size="large" />
-          </View>
-        ) : (
-          days.map((day, index) => (
-            <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.main.temp).toFixed(1)}
-              </Text>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+    );
+  } else
+    return (
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>{city}</Text>
+        </View>
+        <ScrollView
+          // 가로 스크롤
+          horizontal
+          // 스크롤 시 하나씩 넘어가게 설정
+          pagingEnabled
+          // 가로 스크롤바 안보이게 설정
+          showsHorizontalScrollIndicator={false}
+          // 스크롤바 스타일링
+          // indicatorStyle="white"
+          contentContainerStyle={styles.weather}
+        >
+          {days.length === 0 ? (
+            <View style={styles.day}>
+              <ActivityIndicator color="white" size="large" />
             </View>
-          ))
-        )}
-      </ScrollView>
-    </View>
-  );
+          ) : (
+            days.map((day, index) => (
+              <View key={index} style={styles.day}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    width: "100%",
+                  }}
+                >
+                  <Text style={styles.temp}>
+                    {parseFloat(day.main.temp).toFixed(1)}
+                  </Text>
+                  <Ionicons
+                    name={icons[day.weather[0].main]}
+                    size={60}
+                    color="white"
+                  />
+                </View>
+                <Text style={styles.description}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>
+                  {day.weather[0].description}
+                </Text>
+              </View>
+            ))
+          )}
+        </ScrollView>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "tomato",
+  },
+  icon: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   city: {
     flex: 1.2,
@@ -101,23 +142,27 @@ const styles = StyleSheet.create({
   },
   cityName: {
     fontSize: 48,
-    fontWeight: 500,
+    fontWeight: "700",
+    color: "white",
   },
   // NOTE 가로 스크롤 시 flex 사이즈를 주면 넘어가는 부분은 보이지 않으므로 flex를 제거
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    padding: 20,
   },
   temp: {
-    fontSize: 150,
+    fontSize: 120,
     marginTop: 30,
+    color: "white",
   },
   description: {
-    fontSize: 60,
+    fontSize: 40,
     marginTop: -30,
+    color: "white",
   },
   tinyText: {
     fontSize: 20,
+    color: "white",
   },
 });
