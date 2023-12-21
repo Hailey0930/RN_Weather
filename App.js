64e98ec08b6ad06c051e4c16ecab3dbd9c4a1c01
HyerinJeon <hyerin0930@gmail.com>
2023-12-21 14:59:39 +0900

    :sparkles: Layout System

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
+  // NOTE RN에서는 flexDirection 기본값이 column
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 3, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "red",
-  },
-});
